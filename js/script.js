12599f3daf9fcff96bf329b9f4c5e1f541c8dfdd
Joyce Bravo <cjbravo28@gmail.com>
2023-06-20 21:55:40 +0800

    edit css navbars

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 } //
+
+
+
+
+
+
+
+
+
+
